0b2669f0a4789b8d7500e75c93e06e391828cf9e
Kate <katyamalysheva2000@yandex.ru>
2022-09-22 13:19:44 +0300

    model changes

--- shop/stripepay/models.py
+++ shop/stripepay/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=15)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     price_stripe = models.CharField(max_length=100, default="")
 
     def __str__(self) -> str:
